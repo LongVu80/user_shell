f9fef0f7258b2d792f2adc7076f5d1a8156f23fd
LongVu80 <thienlong14th@gmail.com>
2021-05-07 21:03:51 -0500

    This updated branch is for Users_with_Templates assignmentgit add .

--- users_app/views.py
+++ users_app/views.py
@@ -7,3 +7,12 @@ def index(request):
         'all_users': Users.objects.all()
     }
     return render(request, "index.html", context)
+
+def process(request):
+    Users.objects.create(
+        first_name=request.POST['first_name'],
+        last_name=request.POST['last_name'],
+        email_address=request.POST['email_address'],
+        age=request.POST['age']
+        )
+    return redirect('/')
